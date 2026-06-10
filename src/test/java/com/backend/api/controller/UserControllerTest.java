cf91ec07aed175879cdbff1c00d38e3708f228ac
Julian Capilla <julian.capilla@bigtincan.com>
2026-06-10 14:34:45 +0200

    Add User basic tests

--- src/test/java/com/backend/api/controller/UserControllerTest.java
+++ src/test/java/com/backend/api/controller/UserControllerTest.java
@@ -7,7 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDate;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -33,6 +36,102 @@ class UserControllerTest {
         mockMvc.perform(post("/api/users")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").value("John"))
+                .andExpect(jsonPath("$.surname").value("Doe"))
+                .andExpect(jsonPath("$.phone").value("12345678"))
+                .andExpect(jsonPath("$.birthdate").value("1990-05-20"))
+                .andExpect(jsonPath("$.country").value("Denmark"))
+                .andExpect(jsonPath("$.address").value("Some address"));
+    }
+
+
+    @Test
+    void shouldReturnValidationErrors() throws Exception {
+        mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.name")
+                        .value("Name is required"))
+                .andExpect(jsonPath("$.surname")
+                        .value("Surname is required"))
+                .andExpect(jsonPath("$.birthdate")
+                        .value("Date of birth is required"));
+    }
+
+    @Test
+    void shouldRejectInvalidBirthdateFormat() throws Exception {
+        String json = """
+            {
+              "name": "John",
+              "surname": "Doe",
+              "birthdate": "ABC"
+            }
+            """;
+
+        mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.birthdate")
+                        .value("Invalid date format. Expected format: yyyy-MM-dd"));
+    }
+
+    @Test
+    void shouldRejectFutureBirthdate() throws Exception {
+        String json = """
+            {
+              "name": "John",
+              "surname": "Doe",
+              "birthdate": "%s"
+            }
+            """.formatted(
+                LocalDate.now().plusDays(1)
+        );
+
+        mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.birthdate")
+                        .value("Date of birth must be a past date"));
+    }
+
+    @Test
+    void shouldRejectBlankName() throws Exception {
+        String json = """
+            {
+              "name": "",
+              "surname": "Doe",
+              "birthdate": "1990-05-20"
+            }
+            """;
+
+        mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.name")
+                        .value("Name is required"));
+    }
+
+    @Test
+    void shouldRejectBlankSurname() throws Exception {
+        String json = """
+            {
+              "name": "John",
+              "surname": "",
+              "birthdate": "1990-05-20"
+            }
+            """;
+
+        mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.surname")
+                        .value("Surname is required"));
     }
 }
