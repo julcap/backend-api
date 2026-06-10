90ff801c775fc5bbd2268126d95b7ef68a5ac23a
Julian Capilla <julian.capilla@bigtincan.com>
2026-06-10 13:14:54 +0200

    Use lombok getter, setter, builder and constructor generation

--- src/main/java/com/test/api/models/Product.java
+++ src/main/java/com/test/api/models/Product.java
@@ -1,8 +1,16 @@
 package com.test.api.models;
 
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Product {
 
     @Id
@@ -13,47 +21,11 @@ public class Product {
     private String description;
     private Double price;
 
-    // Constructors
-    public Product() {
-        // Empty constructor needed for JPA
-    }
-
+    @Builder
     public Product(String name, String description, Double price) {
         this.name = name;
         this.description = description;
         this.price = price;
     }
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
 }
